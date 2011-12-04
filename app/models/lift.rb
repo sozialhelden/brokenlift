5a89fc5473c35bc16f4c7773e104cb7699807a5e
Janosch Woschitz <janosch.woschitz@gmail.com>
2011-12-04 01:08:59 +0100

    Added operator relation to lift.

--- app/models/lift.rb
+++ app/models/lift.rb
@@ -1,12 +1,14 @@
 class Lift < ActiveRecord::Base
   belongs_to :station
   belongs_to :manufacturer
+  belongs_to :operator
   has_many :events  
 
   acts_as_api
 
   api_accessible :lift_template do |template|
     template.add :id
+    template.add :operator, :template => :simple
     template.add :station, :template => :simple
     template.add :manufacturer, :template => :manufacturer_template
     template.add :description
