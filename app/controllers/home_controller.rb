468f1926ddca58ea94040f56514a83913492ab9a
Matthias Guenther <matthias.guenther@wikimatze.de>
2012-04-15 08:48:36 +0200

    moved the report page into haml

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
   def index
+
+  end
+
+  def about
+  end
+
+  def report
+    @bvg_phonenumber = '+49-30-25622096'
+    @bvg_address = 'http://www.bvg.de/index.php/de/9466/name/Aufzugsmeldungen.html'
+
+    @tam_phonenumber = '+49-30-29743333'
+    @tram_address = 'http://www.s-bahn-berlin.de/kontakt/index.htm'
+
+    @twitter_account = 'https://twitter.com/#!/brokenlifts'
   end
 
 end
+
