0319c98e66e7203424594496f26467ded2fc3816
Janosch Woschitz <janosch.woschitz@gmail.com>
2012-04-15 20:40:19 +0200

    Changed root route to stations index.

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Brokenlifts::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :controller => 'stations', :action => 'index'
 
   # See how all your routes lay out with "rake routes"
 
