cb44aa7351840855a90036fec955df49f455ad4a
Christoph Bünte <info@christophbuente.de>
2012-04-27 20:23:26 +0200

    Use performant db counter queries for start page.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,38 +1,15 @@
 class StationsController < ApplicationController
   def index
-    @stations = Station.find(:all)
-    @stations_broken = []
-    @lifts_broken = 0
-    @lifts_sbahn = 0
-    @lifts_bvg = 0
-    
-    @stations.each_with_index do |station, index|
-      if station.broken?
-          @stations_broken.push(station)
-      end
-      @lifts_broken += station.lifts_broken
-      
-      station.lifts.each do |lift|
-        unless lift.broken?
-          next
-        end
-        
-        if lift.operator.name == "BVG"
-          @lifts_bvg += 1
-        end
-        if lift.operator.name == "S-Bahn"
-          @lifts_sbahn += 1
-        end
-      end
-    end
-    
-    # @todo put the deletion of stations also in loop above
-    @stations_broken.each do |station|
-      @stations.delete(station)
-    end
+    @page = params[:page] || 1
+    @stations_broken = Station.with_lifts.broken
+    @stations = Station.with_lifts - @stations_broken
+    @lifts_broken = Lift.broken.count
+    @lifts_sbahn = Network.find_by_name('S-Bahn').lifts.broken.count
+    @lifts_bvg = Network.find_by_name('BVG').lifts.broken.count
+
   end
-  
+
   def show
-    @station = Station.find(params[:id])    
+    @station = Station.find(params[:id])
   end
 end
